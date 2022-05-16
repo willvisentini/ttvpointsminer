9dd1c1044128ce1c90821a405a050308117e362f
Liack <92930924+Liack731@users.noreply.github.com>
2022-05-15 21:30:02 -0300

    Update run.py

--- run.py
+++ run.py
@@ -13,8 +13,8 @@ from TwitchChannelPointsMiner.classes.entities.Bet import Strategy, BetSettings,
 from TwitchChannelPointsMiner.classes.entities.Streamer import Streamer, StreamerSettings
 
 twitch_miner = TwitchChannelPointsMiner(
-    username="liack731",
-    password="dhO6s1qXdKy5JehAg9eaj54klJ9id8KTuRS3z1QH0c640UGNbv6c4ufmhOwdy4jr",           # If no password will be provided, the script will ask interactively
+    username="",
+    password="",           # If no password will be provided, the script will ask interactively
     claim_drops_startup=False,                  # If you want to auto claim all drops from Twitch inventory on the startup
     priority=[                                  # Custom priority in this case for example:
         Priority.STREAK,                        # - We want first of all to catch all watch streak from all streamers
